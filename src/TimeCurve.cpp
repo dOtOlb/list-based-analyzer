d20604251639500adfa35a13a597e2603b94af87
dotolb <dotolb@gmail.com>
2020-01-08 23:01:53 +0800

    updated the file

--- src/TimeCurve.cpp
+++ src/TimeCurve.cpp
@@ -4,35 +4,40 @@
 
 using namespace std;
 
+/**
+ * Constructor
+ *
+ * @param
+ */
 TimeCurve::TimeCurve(TimeSeries<Tx>::iterator begin_itr, TimeSeries<Tx>::iterator end_itr, TxSetCalculator calc) {
 	float res_val;
-	
+
 	bool first = true;
 	full_count = 0;
 	count = 0;
-	
+
 	for (; begin_itr != end_itr; begin_itr++) {
 		res_val=calc(*begin_itr);
 		curve[(*begin_itr).end_time]=res_val;
 //		cout << "debug 2: " << (*begin_itr).end_time << "\n";
-		
+
 //		cout << res_val << "\n";
-		
+
 		granularity = (*begin_itr).end_time - (*begin_itr).begin_time;
 		if (first || ((*begin_itr).begin_time < start_time)) start_time = (*begin_itr).begin_time;
 		if (first || ((*begin_itr).end_time > end_time)) end_time = (*begin_itr).end_time;
-		
+
 		full_count++;
-		
+
 //		cout << "debug: " << full_count << "\n";
-		
+
 		if (res_val != -1) {
 			count++;
 			if (first || (res_val < min)) min = res_val;
 			if (first || (res_val > max)) max = res_val;
 			avg = first ? res_val : avg + res_val;
 		}
-		
+
 		first = false;
 	}
 //	cout << count << " " << full_count << "\n";
@@ -73,15 +78,15 @@ double TimeCurve::getGranularity() const {
 
 string TimeCurve::toString() const {
 	stringstream output_ss;
-	output_ss << 
+	output_ss <<
 	"Max: " << fixed << setprecision(2) << getMax() << "\n" <<
 	"Min: " << fixed << setprecision(2) << getMin() << "\n" <<
 	"Avg: " << fixed << setprecision(2) << getAvg() << "\n" <<
 	"Data:\n";
-	
-	for (map<double, float>::const_iterator itr = curve.begin(); itr != curve.end(); itr++) { 
+
+	for (map<double, float>::const_iterator itr = curve.begin(); itr != curve.end(); itr++) {
 		output_ss << fixed << setprecision(0) << itr->first << "," << setprecision(2) << itr->second << "\n";
-	}	
-	
+	}
+
 	return output_ss.str();
 }
