372abab4f58bcbbca75e476014e91ee13ff66316
dotolb <dotolb@gmail.com>
2020-01-11 14:37:55 +0800

    fixed the spacing

--- src/TimeSeries.h
+++ src/TimeSeries.h
@@ -114,7 +114,7 @@ public:
 		this->max_slot = new_max_slot;
 	}
 
-    void remove(T obj, double begin_time=0, double end_time=0) {
+  void remove(T obj, double begin_time=0, double end_time=0) {
 		typename TimeSeries<T>::iterator begin_itr = begin_time==0 ? this->begin() : this->begin(begin_time);
 		typename TimeSeries<T>::iterator end_itr = end_time==0 ? this->end() : this->end(end_time);
 
